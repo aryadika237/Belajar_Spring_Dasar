aa1be15bcf530e107b17e66829ceba8ff52b0cfa
Meeno14 <helmi.abdillah01@gmail.com>
2022-03-07 10:13:01 +0700

    Belajar_Spring_Dasar

---
+++ src/main/java/programernow/spring/core/HelloConfiguration.java
@@ -0,0 +1,7 @@
+package programernow.spring.core;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+}
---
+++ src/test/java/programernow/spring/core/ApplicationContextTest.java
@@ -0,0 +1,18 @@
+package programernow.spring.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextTest {
+
+    @Test
+    void testApplicationContext(){
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(HelloConfiguration.class);
+
+        Assertions.assertNotNull(context);
+
+    }
+}
